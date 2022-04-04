11f631cf0203838f22d40f967e474df91f9a1b36
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2022-04-04 12:21:57 +0530

    Almost complete

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,20 +1,7 @@
 import Logo from '../assets/Logo.svg';
-import homebg from '../assets/Background.png';
-import service1 from '../assets/Service1.png';
-import service2 from '../assets/Service2.png';
-import service3 from '../assets/Service3.png';
 import about from '../assets/about.png';
 import leaf from '../assets/leaf.png';
-import art from '../assets/art.png';
-import cit from '../assets/cit.png';
-import datascience from '../assets/datascience.png';
-import globalsystem from '../assets/global.png';
-import graphic from '../assets/graphic.png';
-import music from '../assets/music.png';
-import webdev from '../assets/webdev.png';
-import techinfo from '../assets/techinfo.png';
 import logo2 from '../assets/Logo2.png';
-
 import homebg1 from '../assets/Homebg1.png';
 import Homebg2 from '../assets/homebg2.png';
 import accounting from '../assets/Accounting.png';
@@ -30,22 +17,9 @@ import contactbg from '../assets/contactbg.png';
 
 export default {
     Logo,
-    homebg,
-    service1,
-    service2,
-    service3,
     about,
     leaf,
-    art,
-    cit,
-    datascience,
-    globalsystem,
-    graphic,
-    music,
-    webdev,
-    techinfo,
     logo2,
-
     homebg1,
     Homebg2,
     accounting,
